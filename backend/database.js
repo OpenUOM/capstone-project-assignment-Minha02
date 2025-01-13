f0f579c6663df131f499a4b2c4167310f47bc573
Minha02 <107417428+Minha02@users.noreply.github.com>
2025-01-13 17:21:07 +0530

    Fourth Commit

--- backend/database.js
+++ backend/database.js
@@ -120,7 +120,7 @@ const readStudentInfo = async (id) => {
     });
 }
 
-const addStudent = async (id, name, age, religion) => {
+const addStudent = async (id, name, age, hometown) => {
     const sql = `INSERT INTO student(id,name,age,hometown) values (?,?,?,?)`
     return new Promise((resolve, reject) => {
         knex_db
