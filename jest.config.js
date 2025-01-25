5a17cda52ada57bafdf3bfdc8642d48f94f8958d
Minha02 <107417428+Minha02@users.noreply.github.com>
2025-01-25 11:40:18 +0530

    packages

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,10 @@ module.exports = async () => {
     return {
       verbose: false,
       testTimeout: 30000,
-      maxWorkers: 1
+      maxWorkers: 1,
+      testEnvironment: 'node',
+      testMatch: ['**/test/**/*.test.js'],
+      setupFilesAfterEnv: ['./test/setup.js']
     };
   };
   
